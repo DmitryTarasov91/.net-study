31aa2a5f2b9b0d74718960e5c26a5b0c03932a91
Dmitry Tarasov <Tdvm92@yandex.ru>
2019-12-21 13:34:59 +0300

    First commit

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary Sue", Email = "MS@email.com", Department = "HR" },
+                new Employee() { Id = 1, Name = "Bob Green", Email = "BG@email.com", Department = "IT" },
+                new Employee() { Id = 1, Name = "Tom Cat", Email = "TC@email.com", Department = "OTHER" },
+            };
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
